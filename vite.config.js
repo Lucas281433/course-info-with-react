0e1c8e4fd793ce1a71690c4f47791f451190aed0
Lucas281433 <lucas2814dev@gmail.com>
2024-12-07 10:48:31 -0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Lucas281433.github.io//'
+  base: 'https://Lucas281433.github.io/course-info-with-react/'
 })
